c562407da07e7d5411cc1429b851c58403d1c262
Oscar <lagartoverde97@gmail.com>
2020-03-02 12:25:50 +0100

    added plugins for the demo

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -2,12 +2,16 @@ export default {
   default: [
     "rdfa-editor-date-overwrite-plugin",
     "rdfa-editor-date-plugin",
-    "rdfa-editor-standard-template-plugin"
+    "rdfa-editor-standard-template-plugin",
+    "rdfa-editor-related-url-plugin",
+    "rdfa-editor-dbpedia-fetcher-plugin"
   ],
   all: [
     "rdfa-editor-date-overwrite-plugin",
     "rdfa-editor-date-plugin",
-    "rdfa-editor-standard-template-plugin"
+    "rdfa-editor-standard-template-plugin",
+    "rdfa-editor-related-url-plugin",
+    "rdfa-editor-dbpedia-fetcher-plugin"
   ],
   none: []
 };
