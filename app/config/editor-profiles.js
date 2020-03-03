0b23bc60e43b10c9d8d755acc6fa13533239e133
Oscar <lagartoverde97@gmail.com>
2020-03-03 12:43:12 +0100

    bumped version of plugins

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -3,21 +3,21 @@ export default {
   // * date-overwrite-plugin : allow the user to update the date when a span with datatype="xsd:date" is found [https://github.com/lblod/ember-rdfa-editor-date-overwrite-plugin]
   // * date-plugin : when the user types a date in the document, asks if we want to insert it as a proper rdfa date [https://github.com/lblod/ember-rdfa-editor-date-plugin]
   // * standard-template-plugin : allow the user to insert template when typing keywords in the editor [https://github.com/lblod/ember-rdfa-editor-standard-template-plugin]
-  // * related-url-plugin : when typing dbp:xxx, searches for xxx on dbpedia and insert the link when found [https://github.com/lblod/ember-rdfa-editor-related-url-plugin]
-  // * dbpedia-fetcher-plugin : fetches info about a dbpedia link and displays them in a card [https://github.com/lblod/ember-rdfa-editor-dbpedia-fetcher-plugin]
+  // * wikipedia-slug-plugin : when typing dbp:xxx, searches for xxx on dbpedia and insert the link when found [https://github.com/lblod/ember-rdfa-editor-wikipedia-slug-plugin]
+  // * dbpedia-info-plugin : fetches info about a dbpedia link and displays them in a card [https://github.com/lblod/ember-rdfa-editor-dbpedia-info-plugin]
   default: [
     "rdfa-editor-date-overwrite-plugin",
     "rdfa-editor-date-plugin",
     "rdfa-editor-standard-template-plugin",
-    "rdfa-editor-related-url-plugin",
-    "rdfa-editor-dbpedia-fetcher-plugin"
+    "rdfa-editor-wikipedia-slug-plugin",
+    "rdfa-editor-dbpedia-info-plugin"
   ],
   all: [
     "rdfa-editor-date-overwrite-plugin",
     "rdfa-editor-date-plugin",
     "rdfa-editor-standard-template-plugin",
-    "rdfa-editor-related-url-plugin",
-    "rdfa-editor-dbpedia-fetcher-plugin"
+    "rdfa-editor-wikipedia-slug-plugin",
+    "rdfa-editor-dbpedia-info-plugin"
   ],
   none: []
 };
---
+++ node_modules/@lblod/ember-rdfa-editor-dbpedia-info-plugin/app/components/editor-plugins/dbpedia-info-card.js
@@ -0,0 +1,1 @@
+export { default } from '@lblod/ember-rdfa-editor-dbpedia-info-plugin/components/editor-plugins/dbpedia-info-card';
