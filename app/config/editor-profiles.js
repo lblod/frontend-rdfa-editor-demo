3be64f49019bccbdbd84143ffdf2768d690c9c8a
Erika Pauwels <erika.pauwels@gmail.com>
2020-03-05 19:19:37 +0100

    Update order of plugins in editor-profiles configuration

--- app/config/editor-profiles.js
+++ app/config/editor-profiles.js
@@ -1,14 +1,16 @@
 export default {
-  // This is where we configure the plugins that are active in the editor
-  // * date-overwrite-plugin : allow the user to update the date when a span with datatype="xsd:date" is found [https://github.com/lblod/ember-rdfa-editor-date-overwrite-plugin]
-  // * date-plugin : when the user types a date in the document, asks if we want to insert it as a proper rdfa date [https://github.com/lblod/ember-rdfa-editor-date-plugin]
-  // * standard-template-plugin : allow the user to insert template when typing keywords in the editor [https://github.com/lblod/ember-rdfa-editor-standard-template-plugin]
-  // * wikipedia-slug-plugin : when typing dbp:xxx, searches for xxx on dbpedia and insert the link when found [https://github.com/lblod/ember-rdfa-editor-wikipedia-slug-plugin]
-  // * dbpedia-info-plugin : fetches info about a dbpedia link and displays them in a card [https://github.com/lblod/ember-rdfa-editor-dbpedia-info-plugin]
+  /**
+   * This is where we configure the plugins that are active in the editor
+   * - date-overwrite-plugin : allow the user to update the date when a span with datatype="xsd:date" is found [https://github.com/lblod/ember-rdfa-editor-date-overwrite-plugin]
+   * - date-plugin : when the user types a date in the document, asks if we want to insert it as a proper rdfa date [https://github.com/lblod/ember-rdfa-editor-date-plugin]
+   * - standard-template-plugin : allow the user to insert template when typing keywords in the editor [https://github.com/lblod/ember-rdfa-editor-standard-template-plugin]
+   * - wikipedia-slug-plugin : when typing dbp:xxx, searches for xxx on dbpedia and insert the link when found [https://github.com/lblod/ember-rdfa-editor-wikipedia-slug-plugin]
+   * - dbpedia-info-plugin : fetches info about a dbpedia link and displays them in a card [https://github.com/lblod/ember-rdfa-editor-dbpedia-info-plugin]
+  */
   default: [
-    //"rdfa-editor-date-overwrite-plugin",
     //"rdfa-editor-date-plugin",
-    "rdfa-editor-standard-template-plugin",
+    //"rdfa-editor-date-overwrite-plugin",
+    //"rdfa-editor-standard-template-plugin",
     // "rdfa-editor-wikipedia-slug-plugin",
     // "rdfa-editor-dbpedia-info-plugin"
   ],
