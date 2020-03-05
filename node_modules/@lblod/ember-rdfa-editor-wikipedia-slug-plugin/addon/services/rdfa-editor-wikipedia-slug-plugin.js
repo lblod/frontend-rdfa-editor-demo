6d323233b05fe50b8d82a2fa45af3b8f946b98d7
Oscar <lagartoverde97@gmail.com>
2020-03-05 12:28:16 +0100

    modified plugin

--- node_modules/@lblod/ember-rdfa-editor-wikipedia-slug-plugin/addon/services/rdfa-editor-wikipedia-slug-plugin.js
+++ node_modules/@lblod/ember-rdfa-editor-wikipedia-slug-plugin/addon/services/rdfa-editor-wikipedia-slug-plugin.js
@@ -38,13 +38,14 @@ const RdfaEditorRelatedUrlPlugin = Service.extend({
       hintsRegistry.removeHintsInRegion( context.region, hrId, this.who );
 
       // add hints for context
-      const test = /dbp:([A-z])/g;
-      let match;
-
-      while( match = test.exec( context.text ) ) {
-        const matchText = match[0];
+      const test = /dbp:([A-z]+)/g;
+      let match = context.text.match(test)
+      console.log(match)
+      if(match) {
+        const matchText = match[0].split(':')[1]
+        const matchIndex = context.text.indexOf(matchText)
         const location = this.normalizeLocation(
-          [ match.index, match.index + match[0].length ],
+          [ matchIndex, matchIndex + match[0].length ],
           context.region );
 
         cards.push( {
