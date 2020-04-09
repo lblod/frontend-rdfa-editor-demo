15d81e4cb022c2fbfb93fa46f70dee0d85d5bb27
Aad Versteden <madnificent@gmail.com>
2020-04-09 18:15:25 +0200

    Converting a few calls into Octane syntax
    Octane looks cleaner.  The demo app itself should follow its
    conventions.  Did not go through other code in this repository which
    might have similar constructs.

--- app/controllers/editor/edit.js
+++ app/controllers/editor/edit.js
@@ -2,15 +2,19 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class EditorEditController extends Controller {
+  /**
+   * Callback for editor initialization.  Supplies us with an editor
+   * interface for the document's contents.
+   */
   @action
   handleRdfaEditorInit(editor) {
     this.set('editor', editor);
   }
 
-  @action
-  /*
-   * Save the document
+  /**
+   * Save the document to the shared backend
    */
+  @action
   async save() {
     let editorDocument = this.model;
     editorDocument.set('modifiedOn', new Date());
--- app/controllers/editor/index.js
+++ app/controllers/editor/index.js
@@ -2,15 +2,20 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 
 export default class EditorIndexController extends Controller {
+  /**
+   * Callback for editor initialization.  Supplies us with an editor
+   * interface for the document's contents.
+   */
   @action
   handleRdfaEditorInit(editor) {
     this.set('editor', editor);
   }
 
-  @action
-  /*
-   * Save a new document
+  /**
+   * Creates a new document in the shared backend and then transitions
+   * to a route for that specific document.
    */
+  @action
   async saveNewDocument() {
     let newEditorDocument = this.store.createRecord('editor-document', {
       title: 'New document',
